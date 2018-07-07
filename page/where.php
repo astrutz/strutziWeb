4d2dc083baca8cb7017a5be74df23b2351262671
astrutz <alex-strutz@t-online.de>
2018-07-07 21:06:28 +0200

    include header and Footer

---
+++ page/where.php
@@ -0,0 +1,25 @@
+<?php include '../partials/header.php'; ?>
+<!--<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <div style="overflow:hidden;height:400px;width:600px;">
+        <div id="map_canvas" style="height:400px;width:600px;">
+
+        </div>
+        <a href="http://www.maps-einbinden.de">google maps für webseite</a>
+    </div>
+    <script type="text/javascript">window.setTimeout("initGmaps();", 300);
+
+    function initGmaps() {
+        var myOptions = {
+            zoom: 15,
+            center: new google.maps.LatLng(52.02067, 8.55000999999993),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        marker = new google.maps.Marker({map: map, position: new google.maps.LatLng(52.02067, 8.55000999999993)});
+        infowindow = new google.maps.InfoWindow({content: "<b>Meine Adresse</b><br>Niedermühlenkamp 69<br>Bielefeld"});
+        google.maps.event.addListener(marker, "click", function () {
+            infowindow.open(map, marker);
+        });
+        infowindow.open(map, marker);
+    }</script>-->
+<?php include '../partials/header.php'; ?>
