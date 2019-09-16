4388b8cb301676ff05ca5def44ec339ac4073d80
astrutz <a.strutz@kernpunkt.de>
2019-09-17 00:34:34 +0200

    Meta Tags for SEO

--- partials/header.php
+++ partials/header.php
@@ -3,10 +3,21 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta name="keywords" content="strutzi, strutz, referee, maps, ref, fvm, berg">
+    <meta name="description" content="This is my own website where you just find some of mine interests like traveling or refereeing">
+    <meta name="subject" content="Own experimental web developent by strutzi">
+    <meta name="language" content="DE">
+    <meta name="robots" content="index,follow">
+    <meta name="author" content="Alex Strutz, info@strutzi.de">
+    <meta name="url" content="https://www.strutzi.de">
+    <meta name="pagename" content="Strutzi - Random Firlefanz">
+    <meta name="coverage" content="Worldwide">
+    <meta name="distribution" content="Global">
+    <meta name="rating" content="General">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Hello World!</title>
+    <title>Strutzi - Random Firlefanz</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
