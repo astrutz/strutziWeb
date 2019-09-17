b04c59ec77e6d14023e5bf0ce6071acbc19a9fa9
astrutz <a.strutz@kernpunkt.de>
2019-09-17 02:38:54 +0200

    Footer with legals

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,48 @@
 </div>
+<!-- Footer -->
+<br>
+<br><br>
+<br>
+<section id="footer" class="green">
+    <div class="container">
+        <div class="row text-center text-xs-center text-sm-left text-md-left">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Das Wichtigste</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="../"><i class="fa fa-angle-double-right"></i>Home</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+                    <li><a href="../page/overview"><i class="fa fa-angle-double-right"></i>Referee</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Travel and maps</a></li>
+                    <li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Funny Placeholder</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Social Media</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="https://www.makeuseof.com/tag/stop-using-facebook/"><i class="fa fa-angle-double-right"></i>Facebook</a></li>
+                    <li><a href="https://twitter.com/der_strutz"><i class="fa fa-angle-double-right"></i>Twitter</a></li>
+                    <li><a href="https://www.instagram.com/der_strutz"><i class="fa fa-angle-double-right"></i>Instagram</a></li>
+                    <li><a href="https://www.youtube.com/user/alex97romaniastyle"><i class="fa fa-angle-double-right"></i>YouTube</a></li>
+                    <li><a href="https://www.reddit.com/user/alexuschi/"><i class="fa fa-angle-double-right"></i>Reddit</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <h5>Weiteres</h5>
+                <ul class="list-unstyled quick-links">
+                    <li><a href="../page/impressum"><i class="fa fa-angle-double-right"></i>Impressum</a></li>
+                    <li><a href="../page/datenschutz"><i class="fa fa-angle-double-right"></i>Datenschutzerklärung</a></li>
+                    <li><a href="../sitemap.xml"><i class="fa fa-angle-double-right"></i>Site Map</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                <p class="h6">&copy Alle Rechte vorbehalten -<a class="text-green ml-2" href="https://www.strutzi.de" target="_blank">Alexander Strutz</a></p>
+            </div>
+            </hr>
+        </div>
+    </div>
+</section>
+<!-- ./Footer -->
 </body>
 </html>
