7e0e4c78a8b50a9311d8eb612762c7559a6d2eee
astrutz <a.strutz@kernpunkt.de>
2019-09-18 18:59:30 +0200

    Template for coding

--- page/coding.php
+++ page/coding.php
@@ -2,4 +2,118 @@
 
     <h1 class="custom-headline">Coding</h1>
 
+    <p>Nicht nur in meinem Studium und meinem Beruf programmiere ich gerne. Am liebsten "baue" ich Software von 0 an,
+        wie man an der Website hier sieht mag ich Backend, tolle Datenverwaltungen und algorithmisches Hexhex; weniger
+        aber Pixelschubsen, Design und Frontend. Aber Bootstrap hält den Laden hier glücklicherweise gut zusammen.
+    </p>
+
+    <h2 class="custom-headline">Skills</h2>
+
+    <p>Das soll hier keine verlogene Xing-Auflistung von Technologien werden. Mehr will ich ein wenig sammeln was ich
+        kann (und konnte).</p>
+
+    <div class="card"  style="width: 32rem">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">Java</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">Gradle</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">Maven</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">Android</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">C</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">OpenGL</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">HTML/CSS/Javascript</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">CMS (Wordpress, FirstSpirit)</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">Versionierung</div>
+                    <div class="col-6">
+                        <img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png"><img src="../img/icons8-stern-24.png">
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <h2 class="custom-headline">Projekte</h2>
+    <p>Hier möchte ich meine Projekte ein wenig auflisten. Wenn möglich, versuche ich hier auch Livedemos zu zeigen oder auf Git-Repositories zu verweisen. Für spannende Projekte bin ich immer zu begeistern, insbeonders in den obigen Technologien, nutze dazu am besten das <a href="../page/kontakt">Kontaktformular</a>.</p>
+
+    <h4 class="custom-headline"><i>Filmfix</i> - Filmvorschläge von einer künstlichen Intelligenz</h4>
+    <p>To be filled soon</p>
+
+    <h4 class="custom-headline"><i>Lighting Up Dennis</i> - Automatische Regulierung der Bildschirmhelligkeit anhand des Wetters</h4>
+    <p>To be filled soon</p>
+
+    <h4 class="custom-headline"><i>CologneCoins</i> - Sehenwürdigkeiten in Köln entdecken und Treuepunkte sammeln</h4>
+    <p>To be filled soon</p>
+
+    <h4 class="custom-headline"><i>Regiofood</i> - Lebensmittellisten und regionale Einkäufe durch künstliche Intelligenz</h4>
+    <p>To be filled soon</p>
+
+    <h4 class="custom-headline"><i>Catfinder</i> - App zur Speicherung des Standortes der Katze</h4>
+    <p>To be filled soon</p>
+
+    <h4 class="custom-headline"><i>Bachelorarbeit</i> - Synchronisierung von analogen und digitalen Scrumboards</h4>
+    <p>To be filled soon</p>
+
+    <h4 class="custom-headline"><i>strutziWeb</i> - Eigenes Website mit dynamischem Content und eigenem Editor</h4>
+    <p>To be filled soon</p>
+
+    <h4 class="custom-headline"><i>Was letzte Preis?</i> - Ebay-Simulator</h4>
+    <p>To be filled soon</p>
+
 <?php include '../partials/footer.php'; ?>
