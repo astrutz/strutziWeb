da9b862aa534dbdeb72ce41cfa5b33d3079a4ef1
astrutz <a.strutz@kernpunkt.de>
2019-09-24 23:13:21 +0200

    Finísh v1

--- index.php
+++ index.php
@@ -4,9 +4,17 @@ include 'partials/header.php';
 
 <h1 class="custom-headline">Welcome to strutzi Web!</h1>
 
-<div class="alert alert-danger" role="alert">
-    Unter Konstruktion
-</div>
+<img src="img/penguin.gif">
+
+
+<h2 class="custom-headline">Hello Human! Wo gehts los?</h2>
+
+<p>Exquisit, dass du dich auf dieses glamoröse Stück Internetgeschichte gewagt hast. Hier gibts nicht nur gute
+    Pinguin-GIFs, sondern tolle Katzenfotos in meiner <a href="page/vita.php">Vita</a>. Falls du wissen willst was ich
+    als <a href="page/overview">Schiedsrichter</a>, <a href="page/darts">Darter</a>, <a
+            href="page/coding">Programmierer</a> oder sich <a href="page/reisen">frei bewegendes menschliches Wesen</a> so reiße, finds raus!<br> Oder
+    <a href="page/kontakt">nerv mich</a>!</p>
+
 
 <?php
 include 'partials/footer.php';
