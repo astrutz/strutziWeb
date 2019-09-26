73f0a1972fc7a6129aa0362c2d2726a52819f169
astrutz <a.strutz@kernpunkt.de>
2019-09-26 19:51:11 +0200

    Make darts dynamic

--- page/darts.php
+++ page/darts.php
@@ -1,273 +1,193 @@
 <?php include '../partials/header.php'; ?>
 
+<?php include '../partials/tools.php';  ?>
+
+<?php
+$team = [];
+$sdc = [];
+$other = [];
+$first = [];
+$second = [];
+$third = [];
+$several = [];
+$tournaments = json_decode(file_get_contents('../data/darts.json'), true);
+foreach ($tournaments as $key => $tournament) {
+    switch ($tournament['playerType']) {
+        case 'team':
+            array_push($team, $tournament);
+            break;
+        case 'sdc':
+            array_push($sdc, $tournament);
+            break;
+        case 'other':
+            array_push($other, $tournament);
+            break;
+    }
+}
+foreach ($tournaments as $key => $tournament) {
+    switch ($tournament['position']) {
+        case 1:
+            array_push($first, $tournament);
+            break;
+        case 2:
+            array_push($second, $tournament);
+            break;
+        case 3:
+            array_push($third, $tournament);
+            break;
+        default:
+            if($tournament['position'] != null)
+            array_push($several, $tournament);
+            break;
+    }
+}
+?>
+
+
     <h1 class="custom-headline">Darts</h1>
 
     <h2 class="custom-headline">Überblick</h2>
-    <p>Die folgende Auflistung soll einen kleinen Überblick über meine bisherigen Ergebnisse geben, da drunter folgen dann (soweit vorhanden) detaillierte Statistiken.</p>
+    <p>Die folgende Auflistung soll einen kleinen Überblick über meine bisherigen Ergebnisse geben, da drunter folgen
+        dann (soweit vorhanden) detaillierte Statistiken.</p>
 
     <h4 class="custom-headline">Erste Plätze</h4>
     <ul>
-        <li><a href="#Berghausen2018">Berghausener Meisterschaften 2018</a></li>
-        <li><a href="#4ClubsCup">4 Clubs Cup 2018</a></li>
-        <li><a href="#Masters2017">Vereinsmasters 2017</a></li>
+        <?php echo getAllPlaces($first, false); ?>
     </ul>
     <h4 class="custom-headline">Zweite Plätze</h4>
     <ul>
-        <li><a href="#Berghausen2019">Berghausener Meisterschaften 2019</a></li>
-        <li><a href="#MixedLiga">Gemischte Liga 2018 / 2019*</a></li>
+        <?php echo getAllPlaces($second, false); ?>
     </ul>
     <h4 class="custom-headline">Dritte Plätze</h4>
     <ul>
-        <li><a href="#Meisterschaften2019">Vereinsmeisterschaften 2019</a></li>
-        <li><a href="#Meisterschaften2018">Vereinsmeisterschaften 2018</a></li>
-        <li><a href="#Pokal2018">Vereinspokal 2018</a></li>
-        <li><a href="#Masters2018">Vereinsmasters 2018</a></li>
-        <li><a href="#Trainingscup2018">Trainingscup 2018</a></li>
-        <li><a href="#Pokal2017">Vereinspokal 2017</a></li>
+        <?php echo getAllPlaces($third, false); ?>
     </ul>
     <h4 class="custom-headline">Weitere Platzierungen</h4>
     <ul>
         <li><a href="#Meisterschaften2018">Höchstes Finish (96): Vereinsmeisterschaften 2018</a></li>
-        <li><a href="#BKNS21819">5. Platz: Bezirksklasse Nordrhein-Süd 2 2018/2019</a></li>
-        <li><a href="#BKNS21718">7. Platz: Bezirksklasse Nordrhein-Süd 2 2017/2018</a></li>
-        <li><a href="#Trainingscup2017">7. Platz: Trainingscup 2017</a></li>
-        <li><a href="#Gedenkturnier2017">41. Platz: Achim-Wessolowski-Gedenkturnier 2017</a></li>
+        <?php echo getAllPlaces($several, true); ?>
     </ul>
 
+
     <h2 class="custom-headline">Mannschaftswettbewerbe SDC Gummersbach</h2>
-
-    <a name="BKNS21920"></a>
-    <h4 class="custom-headline">Bezirksklasse Nordrhein-Süd 2 2019/2020</h4>
-    <p>Laufende Saison - siehe unter <a
-                href="https://www.nwdv.live/index.php/component/dartliga/?controller=showligagameplan&layout=showdashboard&filVbKey=1&filStaffKey=14#">NWDV</a>
-    </p>
-
-    <a name="BKNS21819"></a>
-    <h4 class="custom-headline">Bezirksklasse Nordrhein-Süd 2 2018/2019</h4>
-
-<?php include "../data/darts/dartsSDC1819.php"; ?>
-
-    <a name="BKNS21718"></a>
-    <h4 class="custom-headline">Bezirksklasse Nordrhein-Süd 2 2017/2018</h4>
-
-<?php include "../data/darts/dartsSDC1718.php"; ?>
+<?php foreach ($team as $key => $season) { ?>
+    <a name="<?php echo $season['short'] ?>"></a>
+    <h4 class="custom-headline"><?php echo $season['name'] ?></h4>
+    <?php if ($season['results'] != null) { ?>
+        <table class="table">
+            <thead>
+            <tr>
+                <?php foreach ($season['tableheads'] as $rowKey => $rowHead) { ?>
+                    <th scope="col"><?php echo $rowHead; ?></th>
+                <?php } ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($season['results'] as $rowKey => $rowValue) { ?>
+                <tr>
+                    <th scope="row"><?php echo $rowValue[0] ?></th>
+                    <td><?php if ($rowValue[1] != "") { ?><img width="32" height="32"
+                                                               src="<?php echo $rowValue[1] ?>"><?php } ?>
+                    </td>
+                    <?php foreach ($rowValue as $teamKey => $teamValue) {
+                        if ($teamKey > 1) { ?>
+                            <td><?php echo $teamValue; ?></td>
+                        <?php } ?>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    <?php } else { ?>
+        <iframe id="blockrandom" style="border-width: 0;" title="Tabellen"
+                src="https://www.nwdv.live/index.php/component/dartliga/?controller=showligagameplan&amp;layout=showdashboard&amp;filVbKey=1&amp;filStaffKey=14#"
+                name="iframe" width="100%" height="650px">
+            Nutze einen ordentlichen Browser!
+        </iframe>
+    <?php } ?>
+<?php } ?>
 
     <h2 class="custom-headline">Einzelwettbewerbe SDC Gummersbach</h2>
-
-    <a name="Meisterschaften2019"></a>
-    <h4 class="custom-headline">Vereinsmeisterschaften 2019</h4>
-    <p>Das traditionell längste Turnier sind die Meisterschaften mit einer Vorrunde als Liga, bevor es in ein K.O.-System geht.</p>
-    <p>
-        <strong>Gruppenphase</strong>
-        <br>1. Holger Wessolowski
-        <br>2. Frank Bauermeister
-        <br>3. Marcel Bauermeister
-        <br>4. Dirk Renneberg
-        <br><i>5. Alex Strutz</i>
-        <br>6. Marco Martin
-        <br>7. Jürgen Martin
-    </p>
-    <p>
-        <strong>Viertelfinale</strong>
-        <br>Holger Wessolowski 5 : 0 freilos
-        <br>Frank Bauermeister 5 : 0 Jürgen Martin
-        <br>Marcel Bauermeister 5 : 4 Marco Martin
-        <br><i>Alex Strutz 5 : 3 Dirk Renneberg</i>
-    </p>
-    <p>
-        <strong>Halbfinale</strong>
-        <br>Frank Bauermeister 5 : 4 Marcel Bauermeister
-        <br><i>Alex Strutz 5 : 3 Holger Wessolowski</i>
-    </p>
-    <p>
-        <strong>Finale</strong>
-        <br>Frank Bauermeister 6 : 1 Holger Wessolowski
-    </p>
-
-
-    <a name="Meisterschaften2018"></a>
-    <h4 class="custom-headline">Vereinsmeisterschaften 2018</h4>
-    <p>Bei diesen Vereinsmeisterschaften konnte ich mir neben dem dritten Platz erstmalig den Pokal für das höchste Finish mit einer 96 holen.</p>
-    <p>
-        <strong>Gruppenphase</strong>
-        <br><i>1. Alex Strutz</i>
-        <br>2. Holger Wessolowski
-        <br>3. Robbi Freytag
-        <br>4. Frank Bauermeister
-        <br>5. Jürgen Martin
-        <br>6. Marcel Bauermeister
-        <br>7. Christian Grothusen
-        <br>8. Marco Martin
-    </p>
-    <p>
-        <strong>Viertelfinale</strong>
-        <br><i>Alex Strutz 5 : 0 Marco Martin</i>
-        <br>Holger Wessolowski 5 : 3 Christian Grothusen
-        <br>Robbi Freytag 5 : 2 Marcel Bauermeister
-        <br>Frank Bauermeister 5 : 3 Marco Martin
-    </p>
-    <p>
-        <strong>Halbfinale</strong>
-        <br><i>Alex Strutz 2 : 5 Frank Bauermeister</i>
-        <br>Holger Wessolowski 5 : 4 Robbi Freytag
-    </p>
-    <p>
-        <strong>Finale</strong>
-        <br>Frank Bauermeister 6 : 2 Holger Wessolowski
-    </p>
-
-    <a name="Pokal2018"></a>
-    <h4 class="custom-headline">Vereinspokal 2018</h4>
-    <p>Der Vereinspokal wird jährlich im Doppel K.O. Modus mit Best of 9 Legs gespielt, wodurch der dritte und vierte
-        Platz eindeutig ermittelt werden kann.</p>
-    <p>
-        <strong>1. Runde</strong>
-        <br>Frank Lommel 5 : 0 Frank Bauermeister
-        <br>Uwe Hardt 5 : 1 Marco Martin
-        <br>Jürgen Martin 5 : 3 Manuel Müller
-        <br><i>Alex Strutz 5 : 2 Holger Wessolowski</i>
-    </p>
-    <p>
-        <strong>2. Runde</strong>
-        <br>Frank Lommel 5 : 4 Uwe Hardt
-        <br><i>Alex Strutz 5 : 3 Jürgen Martin</i>
-    </p>
-    <p>
-        <strong>Einzug Finale</strong>
-        <br><i>Alex Strutz 2 : 5 Frank Lommel</i>
-    </p>
-    <p>
-        <strong>1. Runde Verliererseite</strong>
-        <br>Holger Wessolowski 5 : 2 Manuel Müller
-        <br>Frank Bauermeister 5 : 1 Marco Martin
-    </p>
-    <p>
-        <strong>2. Runde Verliererseite</strong>
-        <br>Uwe Hardt 5 : 4 Holger Wessolowski
-        <br>Frank Bauermeister 5 : 0 Jürgen Martin
-    </p>
-    <p>
-        <strong>3. Runde Verliererseite</strong>
-        <br>Frank Bauermeister 5 : 0 Uwe Hardt
-    </p>
-    <p>
-        <strong>Einzug Finale Verliererseite</strong>
-        <br><i>Alex Strutz 1 : 5 Frank Bauermeister</i>
-    </p>
-    <p>
-        <strong>Finale</strong>
-        <br>Frank Lommel 5 : 3 Frank Bauermeister
-    </p>
-
-    <a name="Pokal2017"></a>
-    <h4 class="custom-headline">Vereinspokal 2017</h4>
-    Leider gibt es hierzu weder Ergebnisse, noch Platzierungen. Nur der Pokal in meinem Schrank sagt, dass ich hier Dritter geworden bin.
-
-    <a name="Masters2018"></a>
-    <h4 class="custom-headline">Vereinsmasters 2018</h4>
-    <p>Bei den Masters kann es schnell vorbei sein, denn sie werden im Single K.O-Modus gespielt, bei Best of 11 Legs und im Finale Best of 13 Legs.</p>
-    <p>
-        <strong>Vorrunde</strong>
-        <br>Robbi Freytag 0 : 6 Frank Lommel
-    </p>
-    <p>
-        <strong>Viertelfinale</strong>
-        <br><i>Alex Strutz 6 : 2 Marco Martin</i>
-        <br>Frank Lommel 6 : 0 Jürgen Martin
-        <br>Holger Wessolowski 6 : 1 Manuel Müller
-        <br>Marcel Bauermeister 6 : 4 Frank Bauermeister
-    </p>
-    <p>
-        <strong>Halbfinale</strong>
-        <br><i>Alex Strutz 2 : 6 Marcel Bauermeister</i>
-        <br>Frank Lommel 6 : 0 Holger Wessolowski
-    </p>
-    <p>
-        <strong>Finale</strong>
-        <br>Frank Lommel 7 : 0 Marcel Bauermeister
-    </p>
-
-    <a name="Masters2017"></a>
-    <h4 class="custom-headline">Vereinsmasters 2017</h4>
-    <p>Die Masters 2017 waren mein erstes Vereinsturnier und stareten direkt mit einem überraschenden Turniersieg.</p>
-    <p>
-        <strong>Vorrunde</strong>
-        <br><i>Robbi Freytag 4 : 6 Alex Strutz</i>
-    </p>
-    <p>
-        <strong>Viertelfinale</strong>
-        <br><i>Alex Strutz 6 : 5 Marcel Bauermeister</i>
-        <br>Holger Wessolowski 6 : 5 Marco Martin
-        <br>Jürgen Martin 4 : 6 Christan Grothusen
-        <br>Frank Bauermeister 5 : 6 Peter Radermacher
-    </p>
-    <p>
-        <strong>Halbfinale</strong>
-        <br><i>Alex Strutz 6 : 5 Holger Wessolowski</i>
-        <br>Christan Grothusen 6 : 4 Peter Radermacher
-    </p>
-    <p>
-        <strong>Finale</strong>
-        <br><i>Alex Strutz 7 : 2 Christian Grothusen</i>
-    </p>
-
-    <a name="Trainingscup2018"></a>
-    <h4 class="custom-headline">Trainingscup 2018</h4>
-    <p>Zu diesem Trainingscup gibt es leider keine Tabellen. Lediglich die ersten drei Plätze (auf der Weihnachtsfeier genannt) sind bekannt.</p>
-    <p>
-        <strong>Platzierungen</strong>
-        <br>1. Frank Bauermeister
-        <br>2. Jürgen Martin
-        <br><i>3. Alex Strutz</i>
-    </p>
-
-    <a name="Trainingscup2017"></a>
-    <h4 class="custom-headline">Trainingscup 2017</h4>
-    <p>Der Trainingscup wird über das ganze Jahr jeden Mittwoch in einzelnen Turnieren gespielt. Am Ende des Jarhes werden die drei Gesamtbesten ausgezeichnet.</p>
-    <?php include "../data/darts/dartsTrainingscup2017.php"; ?>
+<?php foreach ($sdc as $key => $tournament) { ?>
+    <a name="<?php echo $tournament['short'] ?>"></a>
+    <h4 class="custom-headline"><?php echo $tournament['name'] ?></h4>
+    <p><?php echo $tournament['description'] ?></p>
+    <?php if ($tournament['results'] != null) { ?>
+        <?php if ($tournament['tournamentType'] == 'league') { ?>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <?php foreach ($tournament['tableheads'] as $rowKey => $rowHead) { ?>
+                        <th scope="col"><?php echo $rowHead; ?></th>
+                    <?php } ?>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($tournament['results'] as $rowKey => $rowValue) { ?>
+                    <tr>
+                        <th scope="row"><?php echo $rowValue[0] ?></th>
+                        <?php foreach ($rowValue as $teamKey => $teamValue) {
+                            if ($teamKey > 0) { ?>
+                                <td><?php echo $teamValue; ?></td>
+                            <?php } ?>
+                        <?php } ?>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        <?php } else if ($tournament['tournamentType'] == 'ko') { ?>
+            <?php foreach ($tournament['results'] as $roundKey => $roundValue) { ?>
+                <p>
+                    <strong><?php echo $roundValue['roundName'] ?></strong>
+                    <?php foreach ($roundValue['games'] as $gameKey => $gameValue) { ?>
+                        <br><?php echo $gameValue; ?>
+                    <?php } ?>
+                </p>
+            <?php } ?>
+        <?php } ?>
+    <?php } ?>
+<?php } ?>
 
     <h2 class="custom-headline">Sonstige Einzelwettbewerbe</h2>
 
-    <a name="Berghausen2019"></a>
-    <h4 class="custom-headline">Berghausener Meisterschaften 2019</h4>
-    <p>Leider gibt es bei den Berghausener Turnieren keine Mitschriften, somit kann ich nur vom Finale berichten.</p>
-    <p><strong>Finale</strong>
-        <br><i>Frank Lommel 7 : 3 Alex Strutz</i>
-    </p>
-
-    <a name="Berghausen2018"></a>
-    <h4 class="custom-headline">Berghausener Meisterschaften 2018</h4>
-    <p>Leider gibt es bei den Berghausener Turnieren keine Mitschriften, somit kann ich nur ab dem Halbfinale
-        berichten.</p>
-    <p>
-        <strong>Halbfinale</strong>
-        <br><i>Adrian Burmeister 5 : 6 Alex Strutz</i>
-        <br>Raphael Müller 3 : 6 Peter Kinateder
-    </p>
-    <p>
-        <strong>Finale</strong>
-        <br><i>Alex Strutz 7 : 2 Peter Kinateder</i>
-    </p>
-
-    <a name="4ClubsCup"></a>
-    <h4 class="custom-headline">4 Clubs Cup 2018</h4>
-    <p>Der 4 Clubs Cup setzt sich aus Spielern der Vereine Heier Surround Stars, Stülinghausen 26ers, Heier Ducks und
-        VfL Berghausen zusammen. Gespielt wurde über mehrere Monate an verschiedenen Orten, Modus war Single K.O.</p>
-
-<?php include "../data/darts/darts4ClubsCup.php"; ?>
-
-    <a name="MixedLiga"></a>
-    <h4 class="custom-headline">Gemischte Liga 2018 / 2019</h4>
-
-    <p>*Nach Abschluss des 4 Clubs Cup wurde von den vier Vereinen eine neue Liga geschaffen, um sich auch weiter messen
-        zu können. Leider wurde sie abgebrochen, da einige Spieler es nicht schafften ihre Matches rechtzeitig zu
-        absolvieren. Somit ist diese Tabelle das vorläufige Endergebnis.</p>
-
-<?php include '../data/darts/dartsMixedLeagueTable.php' ?>
-
-    <a name="Gedenkturnier2017"></a>
-    <h4 class="custom-headline">Achim-Wessolowski-Gedenkturnier 2017</h4>
-    <p>Leider gibt es hierzu keine Aufzeichnung mehr. Der Modus war eine Gruppenphase mit 5 Spielern pro Gruppe. In
-        dieser Gruppe wurde ich Letzter und somit 41. von 47, zusammen mit den anderen Gruppenletzten.</p>
+<?php foreach ($other as $key => $tournament) { ?>
+    <a name="<?php echo $tournament['short'] ?>"></a>
+    <h4 class="custom-headline"><?php echo $tournament['name'] ?></h4>
+    <p><?php echo $tournament['description'] ?></p>
+    <?php if ($tournament['results'] != null) { ?>
+        <?php if ($tournament['tournamentType'] == 'league') { ?>
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <?php foreach ($tournament['tableheads'] as $rowKey => $rowHead) { ?>
+                        <th scope="col"><?php echo $rowHead; ?></th>
+                    <?php } ?>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($tournament['results'] as $rowKey => $rowValue) { ?>
+                    <tr>
+                        <th scope="row"><?php echo $rowValue[0] ?></th>
+                        <?php foreach ($rowValue as $teamKey => $teamValue) {
+                            if ($teamKey > 0) { ?>
+                                <td><?php echo $teamValue; ?></td>
+                            <?php } ?>
+                        <?php } ?>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        <?php } else if ($tournament['tournamentType'] == 'ko') { ?>
+            <?php foreach ($tournament['results'] as $roundKey => $roundValue) { ?>
+                <p>
+                    <strong><?php echo $roundValue['roundName'] ?></strong>
+                    <?php foreach ($roundValue['games'] as $gameKey => $gameValue) { ?>
+                        <br><?php echo $gameValue; ?>
+                    <?php } ?>
+                </p>
+            <?php } ?>
+        <?php } ?>
+    <?php } ?>
+<?php } ?>
 
 <?php include '../partials/footer.php'; ?>
