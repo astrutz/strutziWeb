fcedb29111bf2e8f8bbfaf8a691c01c6c1db1c5a
astrutz <a.strutz@kernpunkt.de>
2019-12-23 14:49:01 +0100

    Hotels from other countries in database

---
+++ christmas/1_introduction.php
@@ -0,0 +1,26 @@
+<?php ?>
+
+<div class="presentWrapper">
+    <h2 style="text-align: center" class="custom-headline">Let's go!</h2>
+    <p>Hast du zu Weihnachten schonmal einen Link bekommen? Ich denke mal nicht und ich hab bisher auch noch keine
+        Website verschenkt. Im Endeffekt ist es das auch gar nicht, die Website ist eine gute Möglichkeit das Verpacken
+        mit Geschenkpapier zu vermeiden.</p>
+    <p>Denn du bekommst von mir einen Kurzurlaub zu zweit für drei Tage geschenkt. Jetzt könnte ich dir einfach einen
+        Gutschein in die Hand drücken und sagen: "So, wo gehts hin?". Wir wissen aber beide, dass es ziemlich tricky ist
+        allein einen Kurzurlaub zu planen und nicht mehr viel mit einem Geschenk zu tun hat. Daher habe ich folgendes
+        Tool entwickelt:</p>
+    <p><b>Der Urlaubomat™</b><br/>Er ist DAS Tool um deinen Urlaub zu planen. Du sagst wo du hin willst und der
+        Urlaubomat™ ermittelt dir automatisch dein Weihnachtsgeschenk und deinen Urlaub. Klicke einfach auf den Button
+        unten und du wirst von selbst verstehen.</p>
+    <p>Viel Spaß damit!</p>
+    <p>P.S: Würdige bitte diesen Schneeflockeneffekt. Du willst nicht wissen wie lang das gedauert hat. Aber wenn ich da
+        jetzt so drauf schaue werde ich es wohl doch auf den nächsten Seiten ausblenden. Nervt schon etwas^^</p>
+    <button style="position:relative; top:50%; left:50%;" class="btn btn-success">Starten</button>
+</div>
+
+<script src="../js/jquery.flurry.js"></script>
+<script>
+    $('.presentWrapper').flurry({
+        color: "lightgrey"
+    });
+</script>
--- weihnachten.php
+++ weihnachten.php
@@ -4,21 +4,7 @@ include 'partials/header.php';
 
 <h1 class="custom-headline" style="margin-bottom: 50px">Frohe Weihnachten ♥</h1>
 
-<div class="presentWrapper">
-    <h2 style="text-align: center" class="custom-headline">Let's go!</h2>
-    <p>Hast du zu Weihnachten schonmal einen Link bekommen? Ich denke mal nicht und ich hab bisher auch noch keine Website verschenkt. Im Endeffekt ist es das auch gar nicht, die Website ist eine gute Möglichkeit das Verpacken mit Geschenkpapier zu vermeiden.</p>
-    <p>Denn du bekommst von mir einen Kurzurlaub zu zweit für drei Tage geschenkt. Jetzt könnte ich dir einfach einen Gutschein in die hand drücken und sagen: "So, wo gehts hin?". Wir wissen aber beide, dass es ziemlich tricky ist allein einen Kurzurlaub zu planen und nicht mehr viel mit einem Geschenk zu tun hat. Daher habe ich folgendes Tool entwickelt:</p>
-    <p>Der Urlaubomat™<br/>Er ist dein Tool, um deinen Urlaub zu planen. Du sagst wo du wann hin willst und der Urlaubomat™ ermittelt dir automatisch dein Weihnachtsgeschenk und deinen Urlaub. Klicke einfach auf den Button unten und du wirst von selbst verstehen.</p>
-    <p>Viel Spaß damit!</p>
-    <p>P.S: Würdige bitte diesen Schneeflockeneffekt. Du willst nicht wissen wie lang das gedauert hat. Aber wenn ich da jetzt so drauf schaue werde ich es wohl doch auf den nächsten Seiten ausblenden. Nervt schon etwas^^</p>
-</div>
-
-<script src="js/jquery.flurry.js"></script>
-<script>
-    $('.presentWrapper').flurry({
-        color: "lightgrey"
-    });
-</script>
+<?php include 'christmas/1_introduction.php'?>
 
 <?php
 include 'partials/footer.php';
